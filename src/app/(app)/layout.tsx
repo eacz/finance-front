08325bb3c444ef07abe8451bc8e308541030c396
Esteban Canteros <estebanacanteros@gmail.com>
2024-09-19 17:58:31 -0300

    add transaction filter component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import { TopMenu } from '@/components'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className='min-h-screen'>
+    <main className='min-h-screen bg-gray-100'>
       <TopMenu />
       {children}
     </main>
--- src/transactions/components/TransactionFilter.tsx
+++ src/transactions/components/TransactionFilter.tsx
@@ -1,3 +1,32 @@
 export const TransactionFilter = () => {
-  return <div className='bg-blue-500 w-full flex shadow'>TransactionFilter</div>
+  return (
+    <form className='flex flex-col gap-3'>
+      <div className='flex flex-row gap-2 w-full h-10'>
+        <div className='flex rounded-md overflow-hidden w-full'>
+          <input type='text' className='w-full rounded-md rounded-r-none border-small' />
+          <button className='bg-secondary text-white px-4 font-semibold py-2 rounded-r-md'>Go</button>
+        </div>
+        <button className='px-2  font-semibold py-2 rounded-md bg-gray-100'>Clear</button>
+      </div>
+      <div className='flex flex-col md:flex-row gap-4 justify-center items-center'>
+        <div className='flex flex-col w-full'>
+          <label htmlFor=''>Account</label>
+          <select className='py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none'>
+            <option>All</option>
+            <option>ARS</option>
+            <option>USD </option>
+            <option>USDT</option>
+          </select>
+        </div>
+        <div className='flex flex-col w-full'>
+          <label htmlFor=''>Category</label>
+          <select disabled className='py-3 px-4 pe-9 block w-full  ounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none'>
+            <option>All </option>
+            <option>Clothes</option>
+            <option>Food</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  )
 }
