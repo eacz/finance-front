0377ecafe6f636b5a45f60b2d11ad6357682d9da
Esteban Canteros <estebanacanteros@gmail.com>
2024-11-05 14:36:59 -0300

    add more icons

--- src/utils/getCategoryIcon.ts
+++ src/utils/getCategoryIcon.ts
@@ -1,4 +1,27 @@
-import { IoFastFoodOutline, IoSquareOutline } from 'react-icons/io5'
+import {
+  IoAirplaneOutline,
+  IoBagOutline,
+  IoBarbellOutline,
+  IoBookOutline,
+  IoCarOutline,
+  IoDocumentOutline,
+  IoDocumentTextOutline,
+  IoFastFoodOutline,
+  IoFitnessOutline,
+  IoGridOutline,
+  IoHammerOutline,
+  IoHomeOutline,
+  IoLaptopOutline,
+  IoLogoSteam,
+  IoMailOutline,
+  IoMusicalNoteOutline,
+  IoPeopleOutline,
+  IoServerOutline,
+  IoShirtOutline,
+  IoStorefrontOutline,
+  IoTicketOutline,
+  IoTvOutline,
+} from 'react-icons/io5'
 
 export const getCategoryIcon = (iconText: avalaibleIcons) => {
   const icon = textToIcon[iconText]
@@ -7,8 +30,28 @@ export const getCategoryIcon = (iconText: avalaibleIcons) => {
 }
 
 const textToIcon = {
-  other: IoSquareOutline,
+  other: IoGridOutline,
   food: IoFastFoodOutline,
+  hammer: IoHammerOutline,
+  home: IoHomeOutline,
+  book: IoBookOutline,
+  store: IoStorefrontOutline,
+  shirt: IoShirtOutline,
+  car: IoCarOutline,
+  people: IoPeopleOutline,
+  heart: IoFitnessOutline,
+  document: IoDocumentOutline,
+  documentText: IoDocumentTextOutline,
+  laptop: IoLaptopOutline,
+  mail: IoMailOutline,
+  steam: IoLogoSteam,
+  music: IoMusicalNoteOutline,
+  db: IoServerOutline,
+  ticket: IoTicketOutline,
+  bag: IoBagOutline,
+  barbell: IoBarbellOutline,
+  tv: IoTvOutline,
+  airplane: IoAirplaneOutline,
 }
 
 export type avalaibleIcons = keyof typeof textToIcon
