b82afc00ca0482204901e56a920d11d92da8de28
Esteban Canteros <estebanacanteros@gmail.com>
2024-11-13 11:42:32 -0300

    render icons on ExpensesChart

--- src/modules/transactions/components/ExpensesChart.tsx
+++ src/modules/transactions/components/ExpensesChart.tsx
@@ -1,11 +1,12 @@
 'use client'
+import { getCategoryIcon } from '@/utils'
 import { PieChart, Pie, Cell, ResponsiveContainer, PieLabel } from 'recharts'
 
 const data = [
-  { name: 'Food', value: 400 },
-  { name: 'Services', value: 300 },
-  { name: 'Taxes', value: 300 },
-  { name: 'Clothes', value: 200 },
+  { name: 'Food', value: 400, icon: 'food' },
+  { name: 'Services', value: 300, icon: 'shirt' },
+  { name: 'Taxes', value: 300, icon: 'store' },
+  { name: 'Clothes', value: 200, icon: 'home' },
 ]
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']
 
@@ -22,23 +23,24 @@ const renderCustomizedLabel = ({
   children,
   ...rest
 }: any) => {
-  console.log(rest)
-
+  
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5
-  const x = cx + radius * Math.cos(-midAngle * RADIAN)
-  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+  const x = cx + radius * Math.cos(-midAngle * RADIAN) -10
+  const y = cy + radius * Math.sin(-midAngle * RADIAN) -10
 
+  const Icon = getCategoryIcon(children)
+  
   return (
-    <text x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central'>
-      {children}
-    </text>
+    <svg x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central' >
+      <Icon size={22} color='white' className='relative'/>
+    </svg>
   )
 }
 
 export const ExpensesChart = () => {
   return (
     <ResponsiveContainer>
-      <PieChart width={400} height={400} onMouseEnter={() => {}} id='chart'>
+      <PieChart width={500} height={500} onMouseEnter={() => {}} id='chart'>
         <Pie
           data={data}
           cx='50%'
@@ -46,11 +48,12 @@ export const ExpensesChart = () => {
           labelLine={false}
           label={renderCustomizedLabel}
           outerRadius={80}
+          innerRadius={30}
           fill='#8884d8'
           dataKey='value'>
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}>
-              {entry.name}
+              {entry.icon}
             </Cell>
           ))}
         </Pie>
